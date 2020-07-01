d478d72ca14846fe2cbaabad04362ae0190ac316
me <me@me.me.com>
2020-07-01 15:16:29 -0700

    use const

--- __test__/SGAC.test.js
+++ __test__/SGAC.test.js
@@ -2,20 +2,20 @@ import {SuperGalacticAgeCalculator} from '../src/SuperGalacticAgeCalculator.js'
 
 describe('SuperGalacticAgeCalculator', () => {
   test('should create an empty SuperGalacticAgeCalculatorObject of type object', () => {
-    let sgac = new SuperGalacticAgeCalculator()
+    const sgac = new SuperGalacticAgeCalculator()
 
     expect((typeof sgac)).toEqual("object");
   });
 
   test('should correctly multiply age by a decimal based value', () => {
-    let sgac = new SuperGalacticAgeCalculator()
+    const sgac = new SuperGalacticAgeCalculator()
 
 
     expect(sgac.multiplyAge(4, 1.5)).toEqual(6);
   });
 
   test('should correctly multiply age according to planet', () => {
-    let sgac = new SuperGalacticAgeCalculator()
+    const sgac = new SuperGalacticAgeCalculator()
 
 
     expect(sgac.mercuryAge(4)).toEqual(4 * .24);
@@ -25,22 +25,22 @@ describe('SuperGalacticAgeCalculator', () => {
   });
 
   test('return number of years left in life with a given life expectancy and age', () => {
-    let sgac = new SuperGalacticAgeCalculator()
+    const sgac = new SuperGalacticAgeCalculator()
     
     expect(sgac.yearsLeft(10, 15)).toEqual(5);
   });
 
   test('return a life expectancy value for given demographics of sex, developed country and is american', () => {
-    let sgac = new SuperGalacticAgeCalculator()
+    const sgac = new SuperGalacticAgeCalculator()
     
     expect(sgac.lifeExpectancy("male",true,true,true)).toEqual(75);
   
   });
 
   test('return a years of life left for given demographics on different planets', () => {
-    let sgac = new SuperGalacticAgeCalculator()
-    let age = 30;
-    let lifeExpectancy = sgac.lifeExpectancy("male",true,true,true)
+    const sgac = new SuperGalacticAgeCalculator()
+    const age = 30;
+    const lifeExpectancy = sgac.lifeExpectancy("male",true,true,true)
     expect(sgac.mercuryLifeLeft(lifeExpectancy, age)).toEqual(45 * .24);
     expect(sgac.venusLifeLeft(lifeExpectancy, age)).toEqual(45 * .62);
     expect(sgac.marsLifeLeft(lifeExpectancy, age)).toEqual(45 * 1.88);
